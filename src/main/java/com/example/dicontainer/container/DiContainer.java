b5a573b6dd9905c282f550329f6134d55d278c9c
chunhodong <yhms4432@naver.com>
2023-03-10 23:53:41 +0900

    refactor: DI컨테이너에서 빈즈객체 해시셋으로 초기화

--- src/main/java/com/example/dicontainer/container/DiContainer.java
+++ src/main/java/com/example/dicontainer/container/DiContainer.java
@@ -1,46 +1,52 @@
 package com.example.dicontainer.container;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DiContainer {
 
     private static final int CONST_SIZE = 1;
-    private final Set<Object> beans;
+    private final Set<Object> beans = new HashSet<>();
 
-    public DiContainer(final Set<Class<?>> classes) {
-        this.beans = createBeans(classes);
+    public DiContainer(Set<Class<?>> classes) {
+        createBeans(classes);
     }
 
-    private Set<Object> createBeans(final Set<Class<?>> classes) {
-        Set<Object> beans = new HashSet<>();
-        for (Class<?> aClass : classes) {
-            beans.add(createInstance(aClass));
-        }
-        return beans;
+    private void createBeans(final Set<Class<?>> classes) {
+        classes.stream().forEach(aClass -> beans.add(createInstance(aClass)));
     }
 
     private Object createInstance(final Class<?> aClass) {
         try {
             Constructor<?>[] constructors = aClass.getDeclaredConstructors();
             validateConstructorSize(constructors);
-            Constructor<?> constructor = constructors[0];
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch(InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            return newInstance(constructors[0]);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void validateConstructorSize(Constructor<?>[] constructors){
-        if(Objects.isNull(constructors)){
+    private Object newInstance(Constructor<?> constructor) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        constructor.setAccessible(true);
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        int size = parameterTypes.length;
+        if (parameterTypes == null || parameterTypes.length == 0) return constructor.newInstance();
+        Object[] arguments = Arrays.stream(parameterTypes)
+                .map(parameterType -> getBean(parameterType))
+                .collect(Collectors.toList()).toArray();
+        return constructor.newInstance(arguments);
+    }
+
+    private void validateConstructorSize(Constructor<?>[] constructors) {
+        if (Objects.isNull(constructors)) {
             throw new NullPointerException();
         }
-        if(constructors.length != CONST_SIZE){
+        if (constructors.length != CONST_SIZE) {
             throw new IllegalArgumentException();
         }
     }
