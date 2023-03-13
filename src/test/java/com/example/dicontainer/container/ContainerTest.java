f63a5a54ef8b7838f38846e73fec623e8b9ab9f0
chunhodong <yhms4432@naver.com>
2023-03-13 20:05:41 +0900

    test: 컨테이너에서 객체생성 검증

--- src/test/java/com/example/dicontainer/container/ContainerTest.java
+++ src/test/java/com/example/dicontainer/container/ContainerTest.java
@@ -1,18 +1,20 @@
 package com.example.dicontainer.container;
 
 import com.example.dicontainer.repository.MemberRepository;
+import com.example.dicontainer.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ContainerTest {
 
     @Test
-    @DisplayName("컨테이너에 클래스타입을 생성자인자로 입력하면 빈객체 생성")
-    void createContainer(){
+    @DisplayName("컨테이너에 회원레파지토리 클래스타입을 생성자인자로 입력하면 빈객체 생성")
+    void createContainerWithMemberRep(){
         var classes = new HashSet<Class<?>>();
         classes.add(MemberRepository.class);
 
@@ -20,5 +22,22 @@ public class ContainerTest {
         MemberRepository memberRepository = container.getBean(MemberRepository.class);
 
         assertThat(memberRepository).isNotNull();
+    }
+
+    @Test
+    @DisplayName("컨테이너에 클래스타입을 생성자인자로 입력하면 빈객체 생성")
+    void createContainer(){
+        var classes = new HashSet<Class<?>>();
+        classes.add(MemberService.class);
+        classes.add(MemberRepository.class);
+
+        DiContainer container = new DiContainer(classes);
+        MemberRepository memberRepository = container.getBean(MemberRepository.class);
+        MemberService MemberService = container.getBean(MemberService.class);
+
+        assertThat(memberRepository).isNotNull();
+        assertAll(
+                () -> assertThat(memberRepository).isNotNull(),
+                () -> assertThat(MemberService).isNotNull());
     }
 }
