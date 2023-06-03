b2cd8f98ec2f340f30a2f67ee26e911f96572deb
chunhodong <yhms4432@naver.com>
2023-06-03 18:56:26 +0900

    init

---
+++ src/main/java/com/example/dicontainer/DicontainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicontainerApplication.class, args);
+    }
+
+}
